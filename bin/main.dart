e7ba1a222ee230ec935d91bc04f31621c0aa6f23
jamal <jamal2011922@gmail.com>
2025-06-26 16:57:28 +0400

    added package manager

--- bin/main.dart
+++ bin/main.dart
@@ -4,8 +4,9 @@ import 'package:flutter_feature_gen/options.dart';
 /// Entry point of the CLI tool.
 /// Accepts a single argument: the feature name.
 /// Example: `flutter_feature_gen meal-plan`
-void main(List<String> args) {
+void main(List<String> args) async{
   final options = parseArguments(args);
+  await PackageManager.ensurePackages(options);
   final generator = FeatureGenerator(options);
   generator.generate();
 }
